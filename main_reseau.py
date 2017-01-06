db874be254f697642c0d772d60ac207656f08207
OCangrand <olivier.cangrand@gmail.com>
2017-01-06 18:48:03 +0100

    Correction du problème de display

--- main_reseau.py
+++ main_reseau.py
@@ -27,11 +27,11 @@ def main():
                 if len(l)==2:
                     joueur1 = l[0]
                     joueur2 = l[1]
-                    joueur1.send(b"Welcome, you're the player 1\n")
-                    joueur2.send(b"Welcome, you're the player 2\n")
+                    joueur1.send(b"Bonjour, vous etes le joueur 1\n")
+                    joueur2.send(b"Bonjour, vous etes le joueur 2\n")
                     print("Les deux joueurs sont arrivés, démarrage du jeu.")
                     grids[0].display()
-                    #joueur1.send(grids[J1].display())
+                    joueur1.send(bytearray(grids[J1].displayInOneString(), 'utf8'))
             
                     while grids[0].gameOver() == -1:
                         if current_player == J1:
@@ -52,9 +52,14 @@ def main():
                             grids[current_player].cells[shot] = current_player
                             grids[0].play(current_player, shot)
                             current_player = current_player%2+1
+                        if current_player == J1:
+                            joueur1.send(bytearray(grids[J1].displayInOneString(), 'utf8'))
+                        else:
+                            joueur2.send(bytearray(grids[J2].displayInOneString(), 'utf8'))
                         grids[0].display()
                     print("game over")
-                    grids[0].display()
+                    joueur1.send(bytearray(grids[0].displayInOneString(), 'utf8'))
+                    joueur2.send(bytearray(grids[0].displayInOneString(), 'utf8'))
                     if grids[0].gameOver() == J1:
                         joueur1.send(b"You WIN !")
                         joueur2.send(b"You LOOSE !")
@@ -63,33 +68,4 @@ def main():
                         joueur2.send(b"You WIN !")
 
 
-    """grids = [grid(), grid(), grid()]
-    current_player = J1
-    grids[J1].display()
-    while grids[0].gameOver() == -1:
-        if current_player == J1:
-            shot = -1
-            while shot <0 or shot >=NB_CELLS:
-                shot = int(input ("Joueur 1, quel case allez-vous jouer ?"))
-        else:
-            shot = -1
-            while shot <0 or shot >=NB_CELLS:
-                shot = int(input ("Joueur 2, quel case allez-vous jouer ?"))
-        if (grids[0].cells[shot] != EMPTY):
-            grids[current_player].cells[shot] = grids[0].cells[shot]
-        else:
-            grids[current_player].cells[shot] = current_player
-            grids[0].play(current_player, shot)
-            current_player = current_player%2+1
-        if current_player == J1:
-            grids[J1].display()
-        else:
-            grids[J2].display()
-    print("game over")
-    grids[0].display()
-    if grids[0].gameOver() == J1:
-        print("You win !")
-    else:
-        print("you loose !")
-"""
 main()
